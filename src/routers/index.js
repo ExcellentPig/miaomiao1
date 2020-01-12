53414d1684fd23dc6d3d97994e8fdc80b604d595
ExcellentPig <58382766+ExcellentPig@users.noreply.github.com>
2020-01-12 10:22:28 +0800

    add createComponents

---
+++ src/routers/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import movieRouter from './movie'
+import cinemaRouter from './cinema'
+import mineRouter from './mine'
+
+Vue.use(VueRouter)
+
+const routes = [
+	movieRouter,
+	cinemaRouter,
+	mineRouter,
+	{
+		path:'/',
+		redirect:'/movie'
+	}
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
